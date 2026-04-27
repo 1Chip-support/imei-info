44e9bb446def1d2062a4936a1e5bdabc82dca601
AndGhazaryan <andforeyou@gmail.com>
2026-04-27 17:15:51 +0400

    fix api + add price

--- server.js
+++ server.js
@@ -138,19 +138,7 @@ async (req, res) => {
 });
 
 /* =========================
-DELETE ORDER (NEW)
-========================= */
-app.get("/admin/delete/:id", async (req, res) => {
- try {
-   await Check.findByIdAndDelete(req.params.id);
-   res.redirect("/admin");
- } catch (err) {
-   res.status(500).send("Delete error");
- }
-});
-
-/* =========================
-ADMIN PANEL (iOS STYLE + COPY + DELETE)
+ADMIN PANEL (iOS STYLE + COPY)
 ========================= */
 app.get("/admin", async (req, res) => {
  const data = await Check.find().sort({ time: -1 });
@@ -203,10 +191,8 @@ body{
  cursor:pointer;
 }
 
-.delete{
- color:#ff3b30;
- font-weight:600;
- text-decoration:none;
+.copy:active{
+ opacity:0.5;
 }
 </style>
 </head>
@@ -233,10 +219,6 @@ ${data.map(i => `
    <div class="row"><b>Type:</b> ${i.type}</div>
    <div class="row"><b>Status:</b> ${i.status}</div>
    <div class="row"><b>Paid:</b> ${i.paid ? "YES" : "NO"}</div>
-
-   <div class="row">
-     <a class="delete" href="/admin/delete/${i._id}">🗑 Delete</a>
-   </div>
 
  </div>
 `).join("")}
